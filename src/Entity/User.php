fd4db7c37832e8215c5ce2944369179408f94f5a
Filipouzo <philiparnal@gmail.com>
2022-10-04 00:20:52 +0200

    update User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -31,6 +33,24 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(nullable: true)]
     private ?bool $activated = null;
 
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'users')]
+    private ?self $parent = null;
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private Collection $users;
+
+    #[ORM\ManyToOne(inversedBy: 'id_admin')]
+    private ?GlobalOption $globalOption = null;
+
+    #[ORM\ManyToMany(targetEntity: GlobalOption::class, mappedBy: 'id_partner')]
+    private Collection $globalOptions;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+        $this->globalOptions = new ArrayCollection();
+    }
+
+    
 
     public function getId(): ?int
     {
@@ -130,6 +150,87 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setActivated(?bool $activated): self
     {
         $this->activated = $activated;
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): self
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function addUser(self $user): self
+    {
+        if (!$this->users->contains($user)) {
+            $this->users->add($user);
+            $user->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(self $user): self
+    {
+        if ($this->users->removeElement($user)) {
+            // set the owning side to null (unless already changed)
+            if ($user->getParent() === $this) {
+                $user->setParent(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getGlobalOption(): ?GlobalOption
+    {
+        return $this->globalOption;
+    }
+
+    public function setGlobalOption(?GlobalOption $globalOption): self
+    {
+        $this->globalOption = $globalOption;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, GlobalOption>
+     */
+    public function getGlobalOptions(): Collection
+    {
+        return $this->globalOptions;
+    }
+
+    public function addGlobalOption(GlobalOption $globalOption): self
+    {
+        if (!$this->globalOptions->contains($globalOption)) {
+            $this->globalOptions->add($globalOption);
+            $globalOption->addIdPartner($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGlobalOption(GlobalOption $globalOption): self
+    {
+        if ($this->globalOptions->removeElement($globalOption)) {
+            $globalOption->removeIdPartner($this);
+        }
+
         return $this;
     }
 }
